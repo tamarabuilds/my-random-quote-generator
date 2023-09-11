af2f0f4e4be0fa9cf158b1bbc9a4d1710a70c147
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-11 09:38:26 -0700

    added tag property and random background color

--- js/script.js
+++ js/script.js
@@ -15,34 +15,40 @@ const quotes = [
     quote: "Before you criticize a man, walk a mile in his shoes. That way, if he's upset, he's a mile away and you've got his shoes.",
     source: "Johnny Carson",
     citation: "The Tonight Show Starring Johnny Carson",
-    year: 1991
+    year: 1991,
+    tag: "humor"
   },
   {
     quote: "My mother always used to say: The older you get, the better you get, unless you're a banana.",
     source: "Rose (Betty White)",
     citation: "The Golden Girls",
-    year: 1987
+    year: 1987,
+    tag: "humor"
   },
   {
     quote: "I changed all my passwords to 'Incorrect.' So whenever I forget, it will tell me 'Your password is incorrect.'",
     source: "Michael Scott",
-    citation: "The Office"
+    citation: "The Office",
+    tag: "humor"
   },
   {
     quote: "Before you marry a person, you should first make them use a computer with slow Internet to see who they really are.",
-    source: "Will Ferrell"
+    source: "Will Ferrell",
+    tag: "humor"
   },
   {
     quote: "There's nothing simpler than avoiding people you don't like. Avoiding one's friends, that's the real test.",
     source: "Dowager Countess Violet Crawley (Maggie Smith)",
     citation: "Downton Abbey",
-    year: 2014
+    year: 2014,
+    tag: "humor"
   },
   {
     quote: "Does it disturb anyone else that 'The Los Angeles Angels' baseball team translates directly to 'The The Angels Angels'?",
     source: "Neil DeGrasse Tyson",
     citation: "X (Twitter)",
-    year: 2012
+    year: 2012,
+    tag: "humor"
   }
 ]
 
@@ -59,8 +65,20 @@ function getRandomQuote(){
 }
 
 /***
- *  Gets a random quote by calling the getRandomQuote function,
- *  then builds a string of HTML to display the quote and its properties,
+ *  Updates the background to a random background color
+ * 
+ *  No params or returns
+***/
+function getRandomColor(){
+  const randomValue = () => Math.floor( Math.random() * 256)                        // Is it OK practice to call an annonymous function in your function?
+  const randomRGB = `rgb(${randomValue()}, ${randomValue()}, ${randomValue()})`
+
+  return randomRGB
+}
+
+
+/***
+ *  Calls getRandomQuote(), builds HTML string display the quote and its properties,
  *  and finally displays the random quote in the browser.
  * 
  *  No properties or return.
@@ -68,6 +86,8 @@ function getRandomQuote(){
 
 function printQuote(){
   const randomQuote = getRandomQuote()
+  const color = getRandomColor()
+
   let html = `
     <p class="quote">${randomQuote['quote']}</p>
     <p class="source">${randomQuote['source']}`
@@ -79,10 +99,17 @@ function printQuote(){
   if ( randomQuote['year'] ){
     html += `<span class="year"> ${randomQuote['year']} </span>`
   }
+  
+  if ( randomQuote['tag']) {
+    html += `<span class="citation"> ${randomQuote['tag']} </span>`
+  }
 
   html += `</p>`
 
   document.getElementById('quote-box').innerHTML = html
+
+  // Adding the random background color, picked up from https://stackdiary.com/tutorials/how-to-change-background-color-with-javascript/
+  document.body.style.backgroundColor = color
 
   return
 }
